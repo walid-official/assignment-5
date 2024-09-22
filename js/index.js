8b145d2fa3862b48373a332d6691ee9dc803a3dd
walid hasan <walid.official8@gmail.com>
2024-09-23 00:26:47 +0600

    modal and more validations are added

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ const reservedAmount = document.getElementById('reserved-amount');
 const donationAmount1 = document.getElementById('donation1');
 const donationAmount2 = document.getElementById('donation2');
 const donationAmount3 = document.getElementById('donation3');
+const myModal =  document.getElementById('my_modal_1');
 
 let first_card_btn = buttonClickedById('first-card-btn');
 let second_card_btn = buttonClickedById('second-card-btn');
@@ -15,13 +16,17 @@ third_card_btn.addEventListener('click', card3ButtonAction);
 
 function card1ButtonAction(){
     let firstInput = inputValueById('first-input');
-    if(isNaN(firstInput) || firstInput < 0){
+    if(!isNaN(firstInput) && firstInput > 0){
+       myModal.showModal();
+    }
+    else{
         alert('Please Give Digits and Valid Number');
         return;
     }
     donationAmount1.innerText = firstInput;
     reservedAmount.innerText = reservedAmount.innerText - firstInput;
-    document.getElementById('first-input').value = "";
+    let inputValue1 = document.getElementById('first-input');
+    inputValue1.value = ""
 }
 
 
