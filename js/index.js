30332c2315e7338e6121a74ca07106e4b3a56957
walid hasan <walid.official8@gmail.com>
2024-09-23 04:55:04 +0600

    some designs are revised

--- js/index.js
+++ js/index.js
@@ -2,18 +2,20 @@ const reservedAmount = document.getElementById('reserved-amount');
 const donationAmount1 = document.getElementById('donation1');
 const donationAmount2 = document.getElementById('donation2');
 const donationAmount3 = document.getElementById('donation3');
-const myModal =  document.getElementById('my_modal_1');
 const cardContainer = document.getElementById('card-container');
 const historyContainer = document.getElementById('history-container');
 const historyContent = document.getElementById('history-content');
+const myModal =  document.getElementById('my_modal_1');
 
 
-let first_card_btn = buttonClickedById('first-card-btn');
-let second_card_btn = buttonClickedById('second-card-btn');
-let third_card_btn = buttonClickedById('third-card-btn');
-let donate_tab = buttonClickedById('donate-tab');
-let history_tab = buttonClickedById('history-tab');
-let blog_btn = buttonClickedById('blog-btn');
+
+const first_card_btn = buttonClickedById('first-card-btn');
+const second_card_btn = buttonClickedById('second-card-btn');
+const third_card_btn = buttonClickedById('third-card-btn');
+const donate_tab = buttonClickedById('donate-tab');
+const history_tab = buttonClickedById('history-tab');
+const blog_btn = buttonClickedById('blog-btn');
+
 
 
 first_card_btn.addEventListener('click', card1ButtonAction);
@@ -24,6 +26,9 @@ history_tab.addEventListener('click', historyTabButton);
 blog_btn.addEventListener('click', blogNewTabOpen);
 
 
+
+
+//first-function 1
 function card1ButtonAction(){
     let firstInput = inputValueById('first-input');
     document.getElementById('first-input').value = "";
@@ -42,7 +47,7 @@ function card1ButtonAction(){
     let history1 = `
          <div class="border py-5 px-3 mt-3">
           <h2 class="font-bold">${firstInput} Taka is Donated for famine-2024 at Feni, Bangladesh</h2>
-          <p>Date : ${new Date()}</p>
+          <p class="pt-3">Date : ${new Date()}</p>
         </div>
     `
     historyContainer.innerHTML += history1;
@@ -50,6 +55,10 @@ function card1ButtonAction(){
 
 
 
+
+
+
+//second-function 2
 function card2ButtonAction(){
     let secondInput = inputValueById('second-input');
     document.getElementById('second-input').value = "";
@@ -69,7 +78,7 @@ function card2ButtonAction(){
     let history2 = `
         <div class="border py-5 px-3 mt-3">
         <h2 class="font-bold">${secondInput} Taka is Donated for Flood Relief in Feni,Bangladesh</h2>
-        <p>Date : ${new Date()}</p>
+        <p class="pt-3">Date : ${new Date()}</p>
         </div>
     `
     historyContainer.innerHTML += history2;
@@ -77,6 +86,9 @@ function card2ButtonAction(){
 
 
 
+
+
+// Third Function
 function card3ButtonAction(){
     let thirdInput = inputValueById('third-input');
     if(!isNaN(thirdInput) && thirdInput > 0 && reservedAmount.innerText >= thirdInput){
@@ -96,7 +108,7 @@ function card3ButtonAction(){
     let history3 = `
         <div class="border py-5 px-3 mt-3">
         <h2 class="font-bold">${thirdInput}  Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</h2>
-        <p>Date : ${new Date()}</p>
+        <p class="pt-3">Date : ${new Date()}</p>
         </div>
     `
     historyContainer.innerHTML += history3;
@@ -105,6 +117,7 @@ function card3ButtonAction(){
 
 
 
+// fourth Function
 function donateTabButton(){
     history_tab.classList.remove('bg-primary','text-black');
     history_tab.classList.add('text-gray-600');
@@ -117,6 +130,7 @@ function donateTabButton(){
 
 
 
+// fifth Function
 function historyTabButton(){
     history_tab.classList.add('bg-primary','text-black');
     history_tab.classList.remove('text-gray-600');
@@ -128,7 +142,7 @@ function historyTabButton(){
 }
 
 
-
+// sixth Function
 function blogNewTabOpen(){
     window.location.href = './blog.html';
 }
