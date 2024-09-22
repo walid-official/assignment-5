b9acbd1ef599a7c8cd645fa857104a79586825ba
walid hasan <walid.official8@gmail.com>
2024-09-23 01:15:18 +0600

    donate and history tab design is added

--- js/index.js
+++ js/index.js
@@ -7,11 +7,15 @@ const myModal =  document.getElementById('my_modal_1');
 let first_card_btn = buttonClickedById('first-card-btn');
 let second_card_btn = buttonClickedById('second-card-btn');
 let third_card_btn = buttonClickedById('third-card-btn');
+let donate_tab = buttonClickedById('donate-tab');
+let history_tab = buttonClickedById('history-tab');
+
 
 first_card_btn.addEventListener('click', card1ButtonAction);
 second_card_btn.addEventListener('click', card2ButtonAction);
 third_card_btn.addEventListener('click', card3ButtonAction);
-
+donate_tab.addEventListener('click', donateTabButton);
+history_tab.addEventListener('click', historyTabButton);
 
 
 function card1ButtonAction(){
@@ -33,10 +37,13 @@ function card1ButtonAction(){
 
 function card2ButtonAction(){
     let secondInput = inputValueById('second-input');
-    if(isNaN(secondInput)){
-        alert('Please Give Digits');
-        return;
-    }
+    if(!isNaN(secondInput) && secondInput > 0){
+        myModal.showModal();
+     }
+     else{
+         alert('Please Give Digits and Valid Number');
+         return;
+     }
     donationAmount2.innerText = secondInput;
     reservedAmount.innerText = reservedAmount.innerText - secondInput;
     document.getElementById('second-input').value = "";
@@ -46,11 +53,34 @@ function card2ButtonAction(){
 
 function card3ButtonAction(){
     let thirdInput = inputValueById('third-input');
-    if(isNaN(thirdInput)){
-        alert('Please Give Digits');
-        return;
-    }
+    if(!isNaN(thirdInput) && thirdInput > 0){
+        myModal.showModal();
+     }
+     else{
+         alert('Please Give Digits and Valid Number');
+         return;
+     }
     donationAmount3.innerText = thirdInput;
     reservedAmount.innerText = reservedAmount.innerText - thirdInput;
     document.getElementById('first-input').value = "";
+}
+
+
+
+
+function donateTabButton(){
+    history_tab.classList.remove('bg-primary','text-black');
+    history_tab.classList.add('text-gray-600');
+    donate_tab.classList.add('bg-primary','text-black');
+    donate_tab.classList.remove('border-2','text-gray-600');
+}
+
+
+
+
+function historyTabButton(){
+    history_tab.classList.add('bg-primary','text-black');
+    history_tab.classList.remove('text-gray-600');
+    donate_tab.classList.remove('bg-primary');
+    donate_tab.classList.add('border-2','text-gray-600');
 }
